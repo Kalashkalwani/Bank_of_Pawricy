d52edefc23bcca583269a31676d8ff9d16fb1d3c
kalashkalwani <kalashkalwani2000@gmail.com>
2021-03-09 13:21:53 +0530

    production commit

--- SBS/settings.py
+++ SBS/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  os.path.join(BASE_DIR,"static")
+STATIC_ROOT =  os.path.join(BASE_DIR,"staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 django_heroku.settings(locals())
