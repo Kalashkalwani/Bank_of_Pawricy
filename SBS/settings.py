e48ab4bc9e9ce503e5a7bb53707a7316342cfbe6
kalashkalwani <kalashkalwani2000@gmail.com>
2021-03-09 12:16:13 +0530

    first commit

--- SBS/settings.py
+++ SBS/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/.herokuapp.com/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
