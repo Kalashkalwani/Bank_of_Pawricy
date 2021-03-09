54e16025152fe1b04eb15b7128f7931ba8fe6432
kalashkalwani <kalashkalwani2000@gmail.com>
2021-03-09 12:52:40 +0530

    production commit

--- SBS/settings.py
+++ SBS/settings.py
@@ -130,6 +130,6 @@ STATIC_ROOT =  os.path.join(BASE_DIR,"static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static")
 ]
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
